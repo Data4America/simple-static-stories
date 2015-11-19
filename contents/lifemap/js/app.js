6d4327c20054467540084860ba6cc3d4af160f44
Daniyal Nawaz <dara.daniyal@gmail.com>
2015-11-19 10:48:16 +0500

    Changes in lifemap

--- contents/lifemap/js/app.js
+++ contents/lifemap/js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+  var height = $(window).height();
+
+  $('header').height(height);
+
+  $('section').each(function() {
+    if ($(this).find('.lf-milestone').length > 6) {
+    }
+
+    if ($(this).height() < height) {
+      $(this).height(height);
+    }
+  });
+
+  window.sr = new scrollReveal();
+
 });
