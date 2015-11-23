922400b643df7f59d4dbc79ad2ed2850491c228e
Daniyal Nawaz <dara.daniyal@gmail.com>
2015-11-23 22:40:36 +0500

    Changes in data

--- contents/lifemap/js/app.js
+++ contents/lifemap/js/app.js
@@ -6,34 +6,56 @@ $(document).ready(function() {
 
   window.sr = new scrollReveal();
 
-  $('section').each(function() {
-    var height = $(this).height(),
-      width = $(this).width(),
+  $('.lf-timeline:not(.final)').each(function() {
+    var height = $(this).outerHeight(),
+      width = $(this).outerWidth(),
       center = width / 2;
 
     $(this).prepend('<svg width="' + width + '" height="' + height + '"></svg>');
 
     var svg = d3.select($(this).find('svg')[0]);
 
-    var path = 'M719 0 ' +
+    var right = true;
+    var top = $(this).find('.column.grid:eq(0)').position().top - 20;
+    var path = 'M' + center + ' 0 ' +
       // Staring Point
       'V 50 ' +
-      'C ' + center + ' 70, ' + (center + 10) + ' 70, ' + (center + 20) + ' 70 ' +
-      // First Curve
-      'H 1200 ' +
-      'C 1300 70, 1300 310, 1200 310 ' +
-      // Second Curve
-      'H 200' +
-      'C 100 310, 100 540, 200 540' +
-      // Ending Point
-      'H ' + (center - 20) +
-      'C ' + (center) + ' 540, ' + (center) + ' 560, ' + (center) + ' 580 ' +
-      'V 580';
+      'C ' + center + ' ' + top + ', ' + (center + 20) + ' ' + top + ', ' + (center + 40) + ' ' + top + ' ';
+
+    var $grids = $(this).find('.column.grid');
+    $(this).find('.column.grid').each(function(i) {
+      top = $(this).position().top;
+
+      var x = $(this).position().left;
+      if (right) {
+        x += $(this).width();
+      }
+
+      var gHeight = $(this).height();
+      var margin = ($(this).outerHeight(true) - gHeight) / 2;
+      var y = top + gHeight + margin;
+
+      var curve = (gHeight + margin) / 2;
+      path += 'H ' + x;
+      path += 'C ' + (x + (right ? curve : -curve)) + ' ' + (top - 10) + ',' +
+        (x + (right ? curve : -curve)) + ' ' + y + ', ' +
+        x + ' ' + y + ' ';
+
+      if (i === $grids.length - 1) {
+        path += 'H ' + (center + (right ? 40 : -40)) + ' ';
+        path += 'C ' + center + ' ' + (height - 55) + ', ' +
+          center + ' ' + (height - 20) + ', ' +
+          center + ' ' + height + ' ';
+      }
+
+      right = right ? false : true;
+    });
 
     svg.append('path')
       .attr('stroke', 'white')
       .attr('fill', 'transparent')
-      .style('stroke-width', 10)
+      .style('stroke-width', 6)
+      .style('opacity', 0.6)
       .attr('d', path);
   });
 });
