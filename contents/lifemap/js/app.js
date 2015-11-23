d1f050c78cdaf12eab98424ee1936e015392f991
Daniyal Nawaz <dara.daniyal@gmail.com>
2015-11-23 10:43:02 +0500

    Changes in data

--- contents/lifemap/js/app.js
+++ contents/lifemap/js/app.js
@@ -4,15 +4,36 @@ $(document).ready(function() {
 
   $('header').height(height);
 
-  $('section').each(function() {
-    if ($(this).find('.lf-milestone').length > 6) {
-    }
-
-    if ($(this).height() < height) {
-      $(this).height(height);
-    }
-  });
-
   window.sr = new scrollReveal();
 
+  $('section').each(function() {
+    var height = $(this).height(),
+      width = $(this).width(),
+      center = width / 2;
+
+    $(this).prepend('<svg width="' + width + '" height="' + height + '"></svg>');
+
+    var svg = d3.select($(this).find('svg')[0]);
+
+    var path = 'M719 0 ' +
+      // Staring Point
+      'V 50 ' +
+      'C ' + center + ' 70, ' + (center + 10) + ' 70, ' + (center + 20) + ' 70 ' +
+      // First Curve
+      'H 1200 ' +
+      'C 1300 70, 1300 310, 1200 310 ' +
+      // Second Curve
+      'H 200' +
+      'C 100 310, 100 540, 200 540' +
+      // Ending Point
+      'H ' + (center - 20) +
+      'C ' + (center) + ' 540, ' + (center) + ' 560, ' + (center) + ' 580 ' +
+      'V 580';
+
+    svg.append('path')
+      .attr('stroke', 'white')
+      .attr('fill', 'transparent')
+      .style('stroke-width', 10)
+      .attr('d', path);
+  });
 });
